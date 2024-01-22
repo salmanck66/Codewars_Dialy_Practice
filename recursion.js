aaefa39f81824eb31ef80189dc6b774d108a196a
salmanck66 <salmanulfaris.c.k10@gmail.com>
2024-01-22 14:35:23 +0530

    chng

--- recursion.js
+++ recursion.js
@@ -6,7 +6,7 @@ function factorial(n) {
         return n * factorial(n - 1); 
     } 
 } 
-// 5    
+// 5    4  3  2  1
 let num1 = 5; 
 let result = factorial(num1); 
 console.log("The factorial of given number is :" + result);
